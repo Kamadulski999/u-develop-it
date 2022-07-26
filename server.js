3177023f736df0c7afda48175332b495c3e1a86f
Kamadulski999 <104084465+Kamadulski999@users.noreply.github.com>
2022-07-26 14:36:40 -0500

    added votes table

--- server.js
+++ server.js
@@ -1,31 +1,19 @@
 const express = require('express');
-const inputCheck = require('./utils/inputCheck');
 const db = require('./db/connection');
 const apiRoutes = require('./routes/apiRoutes');
 
-
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Use apiRoutes
 app.use('/api', apiRoutes);
 
-
-// Connect to database
-const db = mysql.createConnection( 
-    {
-    host: 'localhost',
-    user: 'root',
-    password: '',
-    database: 'election'
-    },
-    console.log('connected to the election database')
-);
-
-// Not Found response for unmatched routes
-app.use((req, res) => {
+// Default response for any other request (Not Found)
+app.use((req, res) => { 
   res.status(404).end();
 });
 
@@ -37,11 +25,3 @@ db.connect(err => {
     console.log(`Server running on port ${PORT}`);
   });
 });
-
-
-
-  // Default response for any other request (Not Found)
-app.use((req, res) => {
-    res.status(404).end();
-  });
-
